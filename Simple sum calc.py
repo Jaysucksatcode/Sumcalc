65c44f2e58bf1dc9974661beccc70d57c452abec
Jaysucksatcode <145758324+Jaysucksatcode@users.noreply.github.com>
2023-09-29 11:18:05 +0300

    Add files via upload

---
+++ Simple sum calc.py
@@ -0,0 +1,9 @@
+#Jayden Mathenge
+#SCT211-0030/2022
+Name=input("Please enter your name: ")
+Greeting="Hello "
+print(Greeting+Name+" what two values would you like to operate on?")
+x=float(input("input the value of x: "))
+y=float(input("input the value of y: "))
+sum=x+y
+print("Dear "+Name+" Your sum is ", sum)
